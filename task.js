499014410241d5f6a531bed8370f65b76f9ae3ac
tasneem shehadeh <shhadahatasneem@gmail.com>
2023-04-26 23:19:54 +0300

    add task

---
+++ task.js
@@ -0,0 +1,77 @@
+
+// exercise Operators 
+let cash= 1000, 
+liabilities= 500;
+console.log( cash / liabilities);
+
+let revenues= 1000, 
+expenses= 500;
+console.log( revenues - expenses);
+let liabilities1= 1000, 
+equity= 500;
+console.log( liabilities1 + equity);
+
+let profit= 1000, 
+sales= 500;
+console.log(profit* sales);
+let numbers= (9+7+2)
+console.log(numbers/3);
+let price= 150; 
+console.log(price-(price*30)/100);
+let age =20;
+console.log(age >20 || age <30);
+// console.log(_.inRange(20, 18, 30));
+
+
+let number1 = Math.pow(2, 3);
+
+console.log( number1  );
+console.log(10 %4);
+//____________________________________________________________________________________
+// exercise Data Types 
+console.log(typeof(100));
+console.log( typeof(73.9));
+console.log( typeof(NaN));
+console.log( typeof("Water"));
+console.log( typeof(false));
+console.log( typeof(9 != 11));
+console.log( typeof("Orang" + "e"));
+console.log( typeof("Orange" - "s"));
+console.log( typeof("4" + "8"));
+console.log( typeof( "4" - "8"));
+console.log( typeof("name" + 3));
+console.log( typeof("name" - 3));
+console.log( typeof(82 * "word"));
+console.log( typeof(1 + "hello"));
+console.log( typeof("hello" + 1));
+console.log( typeof( "hello" + true));
+console.log( typeof(typeof (Infinity) ));
+console.log( typeof(1 == '1'));
+console.log( typeof(1 === '1'));
+
+//____________________________________________________________________________________
+// exercise Strings 1
+ const  str = "Welcome to Orange";
+ const str1= str.toUpperCase();
+ const str2= str.slice(7,-6).toUpperCase();
+ const str3= str.replace("Welcome to", " hello from");
+ const str4= str.length;
+ const str6="Welcome to";
+ const thing ='"Orange"';
+
+ let text2 ="jorden";
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(str4);
+console.log(str6+ thing );
+
+console.log(str.concat(" "+ text2));
+//____________________________________________________________________________________
+
+// exercise Strings 2
+const myString = "cactus";
+const result = myString.replace("c", "*");
+console.log(result);
+
+
